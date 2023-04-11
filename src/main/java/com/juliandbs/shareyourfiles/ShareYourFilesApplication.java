65aaf5a9bcdc347c10e90fa77a8c63854f0146be
JulianDbs <jdbsildesmode@gmail.com>
2023-04-11 12:49:49 -0300

    First Commit !!!!!!!!

---
+++ src/main/java/com/juliandbs/shareyourfiles/ShareYourFilesApplication.java
@@ -0,0 +1,13 @@
+package com.juliandbs.shareyourfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareYourFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareYourFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juliandbs/shareyourfiles/ShareYourFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliandbs.shareyourfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareYourFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
