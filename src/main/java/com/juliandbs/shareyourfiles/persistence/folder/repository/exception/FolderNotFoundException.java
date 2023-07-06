10c7564fb0c4bdd2e1a7d12fc887753fe44fad49
JulianDbs <jdbsildesmode@gmail.com>
2023-07-06 15:30:31 -0300

    'exception' folder added inside '/src/main/java/com/juliandbs/shareyourfiles/persistence/folder/repository/' directory. 'FolderAlreadyExistsException.java' file added inside '/src/main/java/com/juliandbs/shareyourfiles/persistence/folder/repository/exception/' directory. 'FolderNotFoundException.java' file added inside '/src/main/java/com/juliandbs/shareyourfiles/persistence/folder/repository/exception/' directory. 'UnauthorizedUserException.java' file added inside '/src/main/java/com/juliandbs/shareyourfiles/persistence/folder/repository/exception/' directory.

---
+++ src/main/java/com/juliandbs/shareyourfiles/persistence/folder/repository/exception/FolderNotFoundException.java
@@ -0,0 +1,37 @@
+package com.juliandbs.shareyourfiles.persistence.folder.repository.exception;
+
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ */
+
+/**
+ * This exception must be thrown by the FolderRepository.
+ * @author JulianDbs
+ */
+public class FolderNotFoundException extends Exception {
+
+    public FolderNotFoundException() {super("Folder Not Found");}
+
+    public FolderNotFoundException(String message) {super(message);}
+
+    public FolderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FolderNotFoundException(Throwable cause) {super(cause);}
+}
